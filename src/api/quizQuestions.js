b86d09cf4a6fd6d56467568cd748d08935fbc229
Shreya <shreyagrg09@gmail.com>
2018-12-16 12:39:03 +0530

    question_form

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,425 @@
+let quizQuestions = [
+    {
+        question: "I found it hard to wind down ? ",
+        answers: [
+            {
+                type: "0",
+                content: "0"
+
+            },
+            {
+                type: "1",
+                content: "1"
+            },
+            {
+                type: "2",
+                content: "2"
+            },
+            {
+                type: "3",
+                content: "3"
+            }
+        ]
+    },
+    {
+        question: "I was aware of dryness of my mouth ? ",
+        answers: [
+            {
+                type: "0",
+                content: "0"
+            },
+            {
+                type: "1",
+                content: "1"
+            },
+            {
+                type: "2",
+                content: "2"
+            },
+            {
+                type: "3",
+                content: "3"
+            }
+        ]
+    },
+    {
+        question: "I couldn't seem to experience any positive feeling at all ?",
+        answers: [
+            {
+                type: "0",
+                content: "0"
+            },
+            {
+                type: "1",
+                content: "1"
+            },
+            {
+                type: "2",
+                content: "2"
+            },
+            {
+                type: "3",
+                content: "3"
+            }
+        ]
+    },
+    {
+        question: "I experienced breathing difficulty",
+        answers: [
+            {
+                type: "0",
+                content: "0"
+            },
+            {
+                type: "1",
+                content: "1"
+            },
+            {
+                type: "2",
+                content: "2"
+            },
+            {
+                type: "3",
+                content: "3"
+            }
+        ]
+    },
+    {
+        question: " I found it difficult to work up the initiative to do things ? ",
+        answers: [
+            {
+                type: "0",
+                content: "0"
+            },
+            {
+                type: "1",
+                content: "1"
+            },
+            {
+                type: "2",
+                content: "2"
+            },
+            {
+                type: "3",
+                content: "3"
+            }
+        ]
+    },
+    {
+        question: "I tended to over-react to situations",
+        answers: [
+            {
+                type: "0",
+                content: "0"
+            },
+            {
+                type: "1",
+                content: "1"
+            },
+            {
+                type: "2",
+                content: "2"
+            },
+            {
+                type: "3",
+                content: "3"
+            }
+        ]
+    },
+    {
+        question: "I felt that I was using a lot of nervous energy",
+        answers: [
+            {
+                type: "0",
+                content: "0"
+            },
+            {
+                type: "1",
+                content: "1"
+            },
+            {
+                type: "2",
+                content: "2"
+            },
+            {
+                type: "3",
+                content: "3"
+            }
+        ]
+    },
+    {
+        question: "I was worried about situations in which I might panic and make a fool of myself",
+        answers: [
+            {
+                type: "0",
+                content: "0"
+            },
+            {
+                type: "1",
+                content: "1"
+            },
+            {
+                type: "2",
+                content: "2"
+            },
+            {
+                type: "3",
+                content: "3"
+            }
+        ]
+    },
+    {
+        question: "I felt that I had nothing to look forward to",
+        answers: [
+            {
+                type: "0",
+                content: "0"
+            },
+            {
+                type: "1",
+                content: "1"
+            },
+            {
+                type: "2",
+                content: "2"
+            },
+            {
+                type: "3",
+                content: "3"
+            }
+        ]
+    },
+    {
+        question: "I found myself getting agitated",
+        answers: [
+            {
+                type: "0",
+                content: "0"
+            },
+            {
+                type: "1",
+                content: "1"
+            },
+            {
+                type: "2",
+                content: "2"
+            },
+            {
+                type: "3",
+                content: "3"
+            }
+        ]
+    },
+    {
+        question: "I found it difficult to relax",
+        answers: [
+            {
+                type: "0",
+                content: "0"
+            },
+            {
+                type: "1",
+                content: "1"
+            },
+            {
+                type: "2",
+                content: "2"
+            },
+            {
+                type: "3",
+                content: "3"
+            }
+        ]
+    },
+    {
+        question: "I felt down-hearted and blue",
+        answers: [
+            {
+                type: "0",
+                content: "0"
+            },
+            {
+                type: "1",
+                content: "1"
+            },
+            {
+                type: "2",
+                content: "2"
+            },
+            {
+                type: "3",
+                content: "3"
+            }
+        ]
+    },
+    {
+        question: "I was intolerant of anything that kept me from getting on with what I was doing",
+        answers: [
+            {
+                type: "0",
+                content: "0"
+            },
+            {
+                type: "1",
+                content: "1"
+            },
+            {
+                type: "2",
+                content: "2"
+            },
+            {
+                type: "3",
+                content: "3"
+            }
+        ]
+    },
+    {
+        question: "I felt I was close to panic",
+        answers: [
+            {
+                type: "0",
+                content: "0"
+            },
+            {
+                type: "1",
+                content: "1"
+            },
+            {
+                type: "2",
+                content: "2"
+            },
+            {
+                type: "3",
+                content: "3"
+            }
+        ]
+    },
+    {
+        question: "I was unable to become enthusiastic about anyhting",
+        answers: [
+            {
+                type: "0",
+                content: "0"
+            },
+            {
+                type: "1",
+                content: "1"
+            },
+            {
+                type: "2",
+                content: "2"
+            },
+            {
+                type: "3",
+                content: "3"
+            }
+        ]
+    },
+    {
+        question: "I felt I wasn't much worth a person",
+        answers: [
+            {
+                type: "0",
+                content: "0"
+            },
+            {
+                type: "1",
+                content: "1"
+            },
+            {
+                type: "2",
+                content: "2"
+            },
+            {
+                type: "3",
+                content: "3"
+            }
+        ]
+    },
+    {
+        question: "I felt that I was rather lonely",
+        answers: [
+            {
+                type: "0",
+                content: "0"
+            },
+            {
+                type: "1",
+                content: "1"
+            },
+            {
+                type: "2",
+                content: "2"
+            },
+            {
+                type: "3",
+                content: "3"
+            }
+        ]
+    },
+    {
+        question: "I was aware of the action of my heart in absence of physical exertion",
+        answers: [
+            {
+                type: "0",
+                content: "0"
+            },
+            {
+                type: "1",
+                content: "1"
+            },
+            {
+                type: "2",
+                content: "2"
+            },
+            {
+                type: "3",
+                content: "3"
+            }
+        ]
+    },
+    {
+        question: "I felt scared without any good reason",
+        answers: [
+            {
+                type: "0",
+                content: "0"
+            },
+            {
+                type: "1",
+                content: "1"
+            },
+            {
+                type: "2",
+                content: "2"
+            },
+            {
+                type: "3",
+                content: "3"
+            }
+        ]
+    },
+    {
+        question: "I felt that life was meaningless",
+        answers: [
+            {
+                type: "0",
+                content: "0"
+            },
+            {
+                type: "1",
+                content: "1"
+            },
+            {
+                type: "2",
+                content: "2"
+            },
+            {
+                type: "3",
+                content: "3"
+            }
+        ]
+    }
+];
+
+export default quizQuestions;
